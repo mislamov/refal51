cd145e23f58cde7c0459791a9854c7e7439d3758
islamov.marat <islamov.marat@9ef0cb00-e5c1-11dd-bce9-bf72c0239d3a>
2009-04-16 10:57:31 +0000

    git-svn-id: http://refal51.googlecode.com/svn/trunk@67 9ef0cb00-e5c1-11dd-bce9-bf72c0239d3a

---
+++ gprof/drefgo.bat
@@ -0,0 +1,10 @@
+@echo off
+del  fn.ref.xml
+del  %1.xml
+rem refgo refal_scaner.rsl %1
+copy ..\refal_scaner.rsl .
+..\bin\debug\refal51.exe %1
+rem | grep '####:'
+del refal_scaner.rsl
+pause
+exit
