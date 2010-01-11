e70526eb5c2ce8397db3ec4202c3d4d41811dd14
islamov.marat <islamov.marat@9ef0cb00-e5c1-11dd-bce9-bf72c0239d3a>
2010-01-11 08:50:59 +0000

    + expat
    git-svn-id: http://refal51.googlecode.com/svn/trunk/direfal-010@187 9ef0cb00-e5c1-11dd-bce9-bf72c0239d3a

---
+++ src/SAXLoader_expat.h
@@ -0,0 +1,30 @@
+
+#include "direfal.h"
+
+#include <stdio.h>
+#include <expat/expat.h>
+
+#if defined(__amigaos__) && defined(__USE_INLINE__)
+#include <expat/proto/expat.h>
+#endif
+
+#ifdef XML_LARGE_SIZE
+#if defined(XML_USE_MSC_EXTENSIONS) && _MSC_VER < 1400
+#define XML_FMT_INT_MOD "I64"
+#else
+#define XML_FMT_INT_MOD "ll"
+#endif
+#else
+#define XML_FMT_INT_MOD "l"
+#endif
+
+#define BUFFSIZE        8192
+
+
+
+
+
+char Buff[BUFFSIZE];
+
+int loadModuleFromXmlFile(RefUserModule*, RefProgram*, char* );
+
