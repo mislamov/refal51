77cd27d63c13d4137e291d7d30cbcf2a28be860b
islamov.marat <islamov.marat@9ef0cb00-e5c1-11dd-bce9-bf72c0239d3a>
2010-01-11 08:50:01 +0000

    git-svn-id: http://refal51.googlecode.com/svn/trunk/direfal-010@186 9ef0cb00-e5c1-11dd-bce9-bf72c0239d3a

---
+++ src/SAXLoaderHeap.h
@@ -0,0 +1,109 @@
+#include "DiRefal.h"
+
+class LoaderHeap {
+        std::map<unistring, std::stack<RefObject*> > stackstags;
+
+    public:
+        std::stack<RefChain *>  stckChains;
+
+		RefUserModule *currentModule;
+		RefProgram    *currentProgram;
+		RefUserCondition*  currentCondition;
+        std::stack<unistring >  activeTag;
+        unistring currentchars;
+        unistring templateOrSent;
+
+
+        RefObject *extractValueFromStack(unistring name){
+            std::stack<RefObject*>  &svalue   = stackstags[name];
+            //std::cout << "\n--stackstags["<<name<<"]("<< stackstags[name].size() <<")" << std::flush;
+            #ifdef TESTCODE
+            if (svalue.empty()) SYSTEMERRORn("unexpected extracter query: " << name);
+            #endif
+            RefObject* value = svalue.top();
+            //std::cout << "\n--extractValueFromStack["<<name<<"]::" << value->toString();
+            svalue.pop();
+            //std::cout << "\n"<< "--stackstags["<<name<<"].count="<< (stackstags[name].size()) <<"\n";
+            return value;
+        }
+        RefObject *getValueFromStack(unistring name){
+            std::stack<RefObject*>  &svalue   = stackstags[name];
+            //std::cout << "\n~~getValueFromStack[" << name << "] " << stackstags[name].size() << std::flush;
+            //std::cout << "\n~~getValueFromStack[" << name << "] " << svalue.size() << std::flush;
+            #ifdef TESTCODE
+            //std::cout << "\nsvalue.size()==" << svalue.size() << "   svalue.size()==0:" << bool(svalue.size()==0);
+            if ( svalue.size()==0 )
+                SYSTEMERRORn("unexpected extracter query: " << name << std::flush);
+            #endif
+            RefObject* value = svalue.top();
+            //svalue.pop();
+            //std::cout << "\n~~getValueFromStack::" << value->toString();
+            //std::cout << "\n"<< "~~stackstags["<<name<<"].count="<< (stackstags[name].size()) <<"\n";
+            return value;
+        }
+        void createChainToStack(){
+            stckChains.push(new RefChain(0));
+        }
+		void createSubstitutionToStack(){
+			//stckChains.push(new RefChainConstructor());
+			stckChains.push(new RefChain(0));
+        }
+		void createPatternToStack(){
+			stckChains.push(new RefChain(0));
+        }
+
+        RefChain* extractCurrChainFromStack(){
+            #ifdef TESTCODE
+            if (stckChains.empty()) SYSTEMERRORn("tring extract from empty stckChains");
+            #endif
+            RefChain *r = stckChains.top();
+            stckChains.pop();
+            return r;
+        }
+        RefChain* getCurrChain(){
+            return stckChains.top();
+        }
+        RefChain* getCurrChainNotConstructor(){
+            if (stckChains.empty()) SYSTEMERRORn("tring extract from empty stckChains");
+            RefChain *r = stckChains.top();
+			//if (dynamic_cast<RefChainConstructor*>(r)){
+			if (dynamic_cast<RefChain*>(r)){
+				RefChain *rr = r;
+				stckChains.pop();
+				if (stckChains.empty()) SYSTEMERRORn("tring extract from empty stckChains");
+				r = stckChains.top();
+				//if (dynamic_cast<RefChainConstructor*>(r)) SYSTEMERRORn("two RefChainConstructors in stack!");
+				if (dynamic_cast<RefChain*>(r)) SYSTEMERRORn("two RefChainConstructors in stack!");
+				stckChains.push(rr);
+			}
+			return r;
+        }
+        RefObject* putValueToStack(unistring name, RefObject* o){
+            /** /
+            std::cout<<"\n++putValueToStack(";
+            std::cout<<name;
+            std::cout<<", ";
+            std::cout<<(long)o;
+            std::cout << o->toString();
+            std::cout << ")";// */
+            stackstags[name].push(o);
+            //std::cout << "\n"<< "++stackstags["<<name<<"].count="<< (stackstags[name].size()) <<"\n";
+            return o;
+        }
+        void setChars(unistring ch){
+            currentchars = ch;
+        }
+        unistring getChars(){
+            return currentchars + (currentchars="");
+        }
+
+
+        //RefData *getNewEmptyRefSymbolByTypeName(unistring);
+        RefVariable *getVariableByTypename(unistring vtype, unistring vname);
+
+
+        LoaderHeap(RefUserModule *m, RefProgram *p){
+            currentModule  = m;
+			currentProgram = p;
+        };
+};
