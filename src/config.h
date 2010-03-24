305f656aa931e57e3d81af40389f4380b636ca3b
islamov.marat <islamov.marat@9ef0cb00-e5c1-11dd-bce9-bf72c0239d3a>
2010-03-24 03:46:08 +0000

    git-svn-id: http://refal51.googlecode.com/svn/trunk@222 9ef0cb00-e5c1-11dd-bce9-bf72c0239d3a

--- src/config.h
+++ src/config.h
@@ -21,7 +21,7 @@
 
 #define REFVERSION "D-Refal 0.1.0 pre-alpha  Copyright (c) 2008-2009 Marat Islamov"
 #define TESTCODE
-#define DEBUG
+//#define DEBUG
 //#define UNICODE
 /****************************************************************************************
 * CORE - сердцевина рефал-машины
