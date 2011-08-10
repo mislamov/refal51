cdc926b013bd4afefbdacaf4dbb07a80384b6107
islamov.marat <islamov.marat@9ef0cb00-e5c1-11dd-bce9-bf72c0239d3a>
2011-08-10 06:40:22 +0000

    git-svn-id: http://refal51.googlecode.com/svn/trunk@311 9ef0cb00-e5c1-11dd-bce9-bf72c0239d3a

--- refal2/include/commands.h
+++
@@ -1,9 +0,0 @@
-#ifndef commands_h
-#define commands_h
-
-#include "direfal.h"
-
-bool INC (DataCursor &l, DataCursor r);
-bool SLIDE(DataCursor &curr, const DataCursor end, DataCursor l, const DataCursor r);
-
-#endif
