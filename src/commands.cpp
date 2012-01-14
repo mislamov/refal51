afdeeca09677c995a29f4cd23ed06793978d4691
islamov.marat@gmail.com <islamov.marat@gmail.com@9ef0cb00-e5c1-11dd-bce9-bf72c0239d3a>
2012-01-14 20:39:58 +0000

    fixes
    git-svn-id: http://refal51.googlecode.com/svn/branches/refal2@335 9ef0cb00-e5c1-11dd-bce9-bf72c0239d3a

--- src/commands.cpp
+++ src/commands.cpp
@@ -8,6 +8,18 @@ bool INC (DataCursor &l, DataCursor r){
 
 bool SLIDE(DataCursor &curr, const DataCursor end, DataCursor l, const DataCursor r){
 	if (l==r) return true; // сравнение с пусто подцепочкой
+
+	//todo: обязательно оптимизировать! можно сравнивать целыми контейнерами,
+	do {
+		++l;
+		++curr;
+	} while (equal(curr, l) && curr!=end && l!=r); // все должно совпадать
+
+	return (l==r && equal(curr, r));
+}
+
+bool SLIDE2(DataCursor &curr, const DataCursor end, DataCursor l, const DataCursor r){
+	if (l==r) return true; // сравнение с пусто подцепочкой
 	++l;
 	//todo: обязательно оптимизировать! можно сравнивать целыми контейнерами,
 	while (equal(curr, l) && curr!=end && l!=r) // все должно совпадать
